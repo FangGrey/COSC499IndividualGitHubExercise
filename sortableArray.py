a86c52fcb646353f33019f58b3fb40313065c36a
Garry <78188777+FangGrey@users.noreply.github.com>
2021-09-16 11:30:51 -0700

    Create sortableArray.py

---
+++ sortableArray.py
@@ -0,0 +1,7 @@
+#unsorted string array
+stringArray = ("White", "Black", "Red", "Blue", "Yellow", "Orange", "Grey", "Green", "Pink")
+
+print(stringArray)
+#this code sorts the list alphabetically using the sorted() function
+sortedStrings = sorted(stringArray)
+print(sortedStrings)
